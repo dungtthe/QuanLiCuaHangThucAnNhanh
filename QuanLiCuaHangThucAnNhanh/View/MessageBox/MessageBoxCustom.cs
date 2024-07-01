57638ca61b9d2870ddfa53c6b4ab8f6b0ac8b86d
Dung Tien The <dungtienthe1920@gmail.com>
2024-07-01 23:04:29 +0700

    xây dựng MessageBoxCustom

---
+++ QuanLiCuaHangThucAnNhanh/View/MessageBox/MessageBoxCustom.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiCuaHangThucAnNhanh.View.MessageBox
+{
+    public class MessageBoxCustom
+    {
+        public static int Success = 1;
+        public static int Error = 2;
+        public static void Show(int type, string message)
+        {
+            if (type == Success)
+            {
+                new Success(message).ShowDialog();
+            }
+            else if (type == Error)
+            {
+                new Error(message).ShowDialog();
+            }
+        }
+    }
+}
---
+++ QuanLiCuaHangThucAnNhanh/ViewModel/MessageBoxVM/MessageBoxViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiCuaHangThucAnNhanh.ViewModel.MessageBoxVM
+{
+    public class MessageBoxViewModel
+    {
+        private string text;
+
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+        public MessageBoxViewModel(string _text)
+        {
+            text = _text;
+        }
+    }
+}
