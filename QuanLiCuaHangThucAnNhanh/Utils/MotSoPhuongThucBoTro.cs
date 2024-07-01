0e7e634c853a78e2e6f9c2711a17ed7836a7e98c
Dung Tien The <dungtienthe1920@gmail.com>
2024-07-01 22:20:23 +0700

    add Icon,Image Resource , add util

---
+++ QuanLiCuaHangThucAnNhanh/Utils/MotSoPhuongThucBoTro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiCuaHangThucAnNhanh.Utils
+{
+    public class MotSoPhuongThucBoTro
+    {
+
+
+        //bổ trợ phần mật khẩu cho người dùng
+        public static string MD5Hash(string password)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(password));
+            byte[] result = md5.Hash;
+            StringBuilder strBuilder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+              
+                strBuilder.Append(result[i].ToString("x2"));
+            }
+            return strBuilder.ToString();
+        }
+    }
+}
