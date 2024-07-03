e834c1d97c6f25b9e0237419a95a1807f015db98
Dung Tien The <dungtienthe1920@gmail.com>
2024-07-03 21:19:28 +0700

    xây dựng view lịch sử bán

---
+++ QuanLiCuaHangThucAnNhanh/ViewModel/NguoiDungVM/ThongKeVM/ThongKeViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace QuanLiCuaHangThucAnNhanh.ViewModel.NguoiDungVM.ThongKeVM
+{
+    public class ThongKeViewModel:BaseViewModel
+    {
+
+        #region chọn ngày
+        private DateTime _selectedDateFrom = DateTime.Now.AddDays(-2);
+        public DateTime SelectedDateFrom
+        {
+            get { return _selectedDateFrom; }
+            set
+            {
+                _selectedDateFrom = value;
+                OnPropertyChanged(nameof(SelectedDateFrom));
+
+                Task.Run(async () => { await loadDataForDateChange(); });
+            }
+        }
+
+        private DateTime _selectedDateTo = DateTime.Now;
+        public DateTime SelectedDateTo
+        {
+            get { return _selectedDateTo; }
+            set
+            {
+                _selectedDateTo = value;
+                OnPropertyChanged(nameof(SelectedDateTo));
+
+                Task.Run(async () => { await loadDataForDateChange(); });
+            }
+        }
+
+        #endregion
+
+
+        #region các Icommand 
+        public ICommand LichSuBanCM { get; set; }
+
+
+        #endregion
+
+
+        public ThongKeViewModel()
+        {
+
+            #region Lịch sử bán
+
+            //lịch sử bán
+            LichSuBanCM = new RelayCommand<Frame>((p) => { return true; }, async (p) =>
+            {
+                if (p == null) return;
+
+                p.Content = new View.NguoiDung.ThongKe.LichSuBan.LichSuTable();
+
+
+            });
+            #endregion
+
+        }
+        private async Task loadDataForDateChange()
+        {
+
+        }
+    }
+}
