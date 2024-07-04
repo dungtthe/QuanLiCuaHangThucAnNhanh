1b2d0a063efec3eccf56197fd28db8847a470486
Dung Tien The <dungtienthe1920@gmail.com>
2024-07-04 09:02:41 +0700

    tìm kiếm khách hàng

--- QuanLiCuaHangThucAnNhanh/ViewModel/NguoiDungVM/SaleVM/SaleViewModel.cs
+++ QuanLiCuaHangThucAnNhanh/ViewModel/NguoiDungVM/SaleVM/SaleViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using QuanLiCuaHangThucAnNhanh.Model.DA;
 using QuanLiCuaHangThucAnNhanh.Model.DTO;
+using QuanLiCuaHangThucAnNhanh.View.MessageBox;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace QuanLiCuaHangThucAnNhanh.ViewModel.NguoiDungVM.SaleVM
@@ -28,12 +30,41 @@ namespace QuanLiCuaHangThucAnNhanh.ViewModel.NguoiDungVM.SaleVM
         }
 
 
+
+        private KhachHangDTO khachHangForBill;
+        public KhachHangDTO KhachHangForBill
+        {
+            get => khachHangForBill;
+            set
+            {
+                khachHangForBill = value;
+                OnPropertyChanged(nameof(KhachHangForBill));
+            }
+        }
+
+
+        private KhachHangDTO khachVangLai;
+
+        //text tìm kiếm
+        private string _cusInfo;
+        public string CusInfo
+        {
+            get { return _cusInfo; }
+            set { _cusInfo = value; OnPropertyChanged(); }
+        }
+
+
         public ICommand FirstLoadCM { get; set; }
+        public ICommand SearchCusCM { get; set; }
 
 
 
         public SaleViewModel()
         {
+            KhachHangForBill = new KhachHangDTO();
+            KhachHangForBill.HoTen = "Khách vãng lai";
+            khachVangLai = new KhachHangDTO();
+            khachVangLai.HoTen= "Khách vãng lai";
 
             FirstLoadCM = new RelayCommand<object>((p) => { return true; }, async (p) =>
             {
@@ -50,7 +81,30 @@ namespace QuanLiCuaHangThucAnNhanh.ViewModel.NguoiDungVM.SaleVM
             });
 
 
+            SearchCusCM = new RelayCommand<object>((p) => { return true; }, async (p) =>
+            {
 
+                KhachHangDTO temp = null;
+
+
+                temp = await KhachHangDA.gI().FindNguoiDungBySoDienThoai(CusInfo);
+
+                if (temp == null)
+                {
+                    temp = await KhachHangDA.gI().FindNguoiDungByEmail(CusInfo);
+                }
+
+                if (temp == null)
+                {
+                    Error wd1 = new Error("Khách hàng không tồn tại");
+                    temp = khachVangLai;
+                    wd1.ShowDialog();
+
+                }
+                KhachHangForBill = temp;
+
+               
+            });
         }
     }
 }
